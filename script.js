3797180e28bb82198b10cf4b65e2f1a4780ddb10
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2023-12-24 14:08:36 +0800

    Add game() and variables to count result, add counts in playRound()

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection == "rock" && computerSelection == "Paper") {
+    comWon++;
     return "You lose! Paper beats Rock!"
   } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+    playerWon++;
     return "You won! Rock beats Scissors!"
   } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+    comWon++;
     return "You lose! Scissors beat Paper!"
   } else if (playerSelection == "paper" && computerSelection == "Rock") {
+    playerWon++;
     return "You won! Paper beats Rock!"
   } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+    comWon++;
     return "You lose! Rock beats Scissors!"
   } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+    playerWon++;
     return "You won! Scissors beat Paper!"
   } else {
     return "TIES! Try again!";
@@ -23,7 +29,7 @@ function getComputerChoice() {
 }
 
 function getPlayerSelction() {
-  let choice = prompt("Please choose Rock, Paper or Scissors:");
+  let choice = prompt("Please choose Rock, Paper or Scissors:", "");
   choice = choice.toLowerCase();
   if (choice == "rock" || choice == "paper" || choice == "scissors") {
     return choice;
@@ -32,8 +38,27 @@ function getPlayerSelction() {
     getPlayerSelction();
   }
 }
-   
-let playerSelection = getPlayerSelction();
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+
+let playerWon = 0;
+let comWon = 0;
+let result;
+
+function game() {
+  let flag = true;
+
+  while (flag) {
+    let playerSelection = getPlayerSelction();
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection) + ` It's ${playerWon} to ${comWon} now.`);
+    if (playerWon === 3) {
+      result = `${playerWon} to ${comWon}, You won!`;
+      flag = false;
+    } else if (comWon === 3) {
+      result = `${playerWon} to ${comWon}, You lose!`;
+      flag = false;
+    }
+  }
+  console.log(result);
+}
+
+game();
