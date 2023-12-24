b58eac89e4bccf86e55c3e61385f6107f61434f9
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2023-12-24 13:13:29 +0800

    Fuction getComputerChoice() is Done.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock" && computerSelection == "Paper") {
-      return "You lose! Paper beats Rock!"
-    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-      return "You won! Rock beats Scissors!"
-    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-      return "You lose! Scissors beat Paper!"
-    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-      return "You won! Paper beats Rock!"
-    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-      return "You lose! Rock beats Scissors!"
-    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-      return "You won! Scissors beat Paper!"
-    } else {
-      return "TIES! Try again!";
-    }
+  if (playerSelection == "Rock" && computerSelection == "Paper") {
+      eturn "You lose! Paper beats Rock!"
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "You won! Rock beats Scissors!"
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return "You lose! Scissors beat Paper!"
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "You won! Paper beats Rock!"
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return "You lose! Rock beats Scissors!"
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "You won! Scissors beat Paper!"
+  } else {
+    return "TIES! Try again!";
   }
-function getComputerChoice() {
+}
 
+function getComputerChoice() {
+  let arr = ["Rock", "Paper", "Scissors"];
+  return arr[Math.floor(Math.random()*2)];
 }
    
 const playerSelection = "rock";
