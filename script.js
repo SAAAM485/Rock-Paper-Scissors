21a90bba7477be29d8aa13bab53a1cf414470723
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2024-01-16 23:38:48 +0800

    Re-write game() to bo9Games(), still cannot get event.target.id

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let playerWon = 0;
+let comWon = 0;
+let result;
+
+function getPlayerSelction(event) {
+  let playerSelection;
+  
+  console.log(event.target.innerHTML);
+  switch(event.target.innerHTML){
+    case "Rock":
+      playerSelection = "rock";
+      break;
+    case "Paper":
+       playerSelection = "paper";
+      break;
+    case "Scissors":
+       playerSelection = "scissors"
+      break;
+  }
+
+  return playerSelection;
+}
+
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "Paper") {
     comWon++;
@@ -22,43 +46,34 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function getComputerChoice() {
+function getComputerSelection() {
   let arr = ["Rock", "Paper", "Scissors"];
   return arr[Math.floor(Math.random()*3)];
 }
 
-const rock = document.querySelector("#Rock");
-const paper = document.querySelector("#Paper");
-const scissors = document.querySelector("Scissors");
+const sub = document.querySelector("#sub")
+const card = document.querySelector(".card");
+let score = document.createElement("div");
 
-rock.addEventListener("ckick", getPlayerSelction(this.id))
-paper.addEventListener("ckick", getPlayerSelction(this.id))
-scissors.addEventListener("ckick", getPlayerSelction(this.id))
+sub.addEventListener("click", (event) => {
+  bo9Games(event);
+});
 
-function getPlayerSelction(playerSelection) {
-  return playerSelection.toLowerCase();
-}
-
-let playerWon = 0;
-let comWon = 0;
-let result;
-
-function game() {
-  let flag = true;
-
-  while (flag) {
-    let playerSelection = getPlayerSelction();
-    let computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection) + ` It's ${playerWon} to ${comWon} now.`);
-    if (playerWon === 3) {
-      result = `${playerWon} to ${comWon}, You won!`;
-      flag = false;
-    } else if (comWon === 3) {
-      result = `${playerWon} to ${comWon}, You lose!`;
-      flag = false;
-    }
+function bo9Games(event) {
+  if (playerWon < 5 && comWon < 5) {
+    let playerSelection = getPlayerSelction(event);
+    let computerSelection = getComputerSelection();
+    score.textContent = playRound(playerSelection, computerSelection) + ` It's ${playerWon} to ${comWon} now.`;
+    card.appendChild(score);
+  } else if (playerWon === 5) {
+    result = `${playerWon} to ${comWon}, You won!`;
+    score.textContent = result;
+    card.appendChild(score);
+  } else if (comWon === 5) {
+    result = `${playerWon} to ${comWon}, You lose!`;
+    score.textContent = result;
+    card.appendChild(score);
   }
-  console.log(result);
+  
 }
 
-game();
